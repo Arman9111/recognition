79957825973cbc181edaf48cf103b087e999efec
Arman9111 <94247814+Arman9111@users.noreply.github.com>
2023-05-04 17:45:05 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,2 +1,29 @@
 //homework project 103
 //https://teachablemachine.withgoogle.com/models/MLVyEnk1J/
+
+Webcam.set({
+    width: 350,
+    height: 300,
+    image_format: 'png',
+    png_quality: 90
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach('#camera');
+
+function take_snapshot() {
+    Webcam.snap(function (data_uri) {
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="' + data_uri + '"/>';
+
+    });
+}
+
+console.log('ml5 version', ml5.version);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/rq-FJ3SZ0/model.json', modelLoaded);
+function model_loaded(){
+    console.log("Model has been loaded!")
+}
+function check(){
+    
+}
